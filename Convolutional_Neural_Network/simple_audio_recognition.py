610651697966409da39c700d8042c7d985299da2
Harshini <115682794+harshini5204@users.noreply.github.com>
2024-08-02 01:14:28 +0530

    Create simple_audio_recognition.py

---
+++ Convolutional_Neural_Network/simple_audio_recognition.py
@@ -0,0 +1,65 @@
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+if not data_dir.exists():
+  tf.keras.utils.get_file(
+      'mini_speech_commands.zip',
+      origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+      extract=True,
+      cache_dir='.', cache_subdir='data')
+  commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[(commands != 'README.md') & (commands != '.DS_Store')]
+print('Commands:', commands)
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both')
+
+label_names = np.array(train_ds.class_names)
+print()
+print("label names:", label_names)
+train_ds.element_spec
+def squeeze(audio, labels):
+  audio = tf.squeeze(audio, axis=-1)
+  return audio, labels
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
+
+for example_audio, example_labels in train_ds.take(1):  
+  print(example_audio.shape)
+  print(example_labels.shape)
+  label_names[[1,1,3,0]]
+
+plt.figure(figsize=(16, 10))
+rows = 3
+cols = 3
+n = rows * cols
+for i in range(n):
+  plt.subplot(rows, cols, i+1)
+  audio_signal = example_audio[i]
+  plt.plot(audio_signal)
+  plt.title(label_names[example_labels[i]])
+  plt.yticks(np.arange(-1.2, 1.2, 0.2))
+  plt.ylim([-1.1, 1.1])
